d2cfd7ef7d0af2d15bbba13bd3972671ddf7b009
Remzo <remystuurwold@gmail.com>
2016-04-23 15:38:32 +0200

    player animation

--- Assets/Personal Folders/Remy/Scripts/Camera.cs
+++ Assets/Personal Folders/Remy/Scripts/Camera.cs
@@ -5,8 +5,6 @@ public class Camera : MonoBehaviour {
 
     private const float c_CameraSpeed = 2.0f;
 
-    public GameObject m_PlayerObject;
-
     public Vector3 m_Offset = Vector3.zero;
 
 	// Use this for initialization
@@ -18,7 +16,7 @@ public class Camera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 targetPos = m_PlayerObject.transform.position + m_Offset;
+        Vector3 targetPos = Player.Instance.transform.position + m_Offset;
 
         this.transform.position += (targetPos - this.transform.position) * c_CameraSpeed * Time.deltaTime;
 
