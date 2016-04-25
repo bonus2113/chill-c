44d2710f6aabad390f1492b4efb660c28727ff88
Dario Seyb <dario.seyb@gmail.com>
2016-04-25 18:53:09 +0200

    added framerate to fountain

--- Assets/Scripts/FountainSimulate.cs
+++ Assets/Scripts/FountainSimulate.cs
@@ -13,12 +13,15 @@ public class FountainSimulate : MonoBehaviour {
   public ComputeShader SimulateShader;
   public int Resolution;
   public ParticleSystem particlesToRender;
+  public int FrameRate = 10;
 
   RenderTexture densityTex;
   int densityTransferKernel;
   MeshRenderer meshRend;
 
   ParticleSystem.Particle[] particles;
+
+  float frameTime;
 
   // Use this for initialization
   void Awake ()
@@ -38,6 +41,8 @@ public class FountainSimulate : MonoBehaviour {
     meshRend.material.SetTexture("_DensityTex", densityTex);
 
     particles = new ParticleSystem.Particle[particlesToRender.maxParticles];
+
+    frameTime = 1.0f / FrameRate;
   }
 
 
@@ -45,6 +50,11 @@ public class FountainSimulate : MonoBehaviour {
   // Update is called once per frame
   void Update ()
   {
+    frameTime -= Time.deltaTime;
+    if (frameTime > 0) return;
+
+    frameTime = 1.0f / FrameRate;
+
     int count = particlesToRender.GetParticles(particles);
 
     if (count != 0)
