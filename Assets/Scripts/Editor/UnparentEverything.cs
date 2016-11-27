888257fd19a88290caffdf9822e384c614a2c38b
Remzo <remystuurwold@gmail.com>
2016-11-27 15:51:08 +0100

    editor script to fix gian's bullshit (Unparent all children recursively)

---
+++ Assets/Scripts/Editor/UnparentEverything.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class UnparentEverything : MonoBehaviour {
+
+    [MenuItem("Grove/FixBullshit/UnparentAll")]
+    static void UnparentAllChildren() //create orphans lol
+    {
+        var selectionTransform = Selection.activeTransform;
+
+        UnparentChildren(selectionTransform);
+    }
+
+    static void UnparentChildren(Transform parent)
+    {
+        while (parent.childCount != 0)
+        {
+            Transform child = parent.GetChild(0);
+            child.SetParent(null); //unparent child
+            //unparent child's children
+            UnparentChildren(child);
+        }
+    }
+}
