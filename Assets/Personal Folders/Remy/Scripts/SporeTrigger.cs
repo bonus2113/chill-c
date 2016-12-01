f7c673f34d41db1ca7df60173135d4062002efcf
Remzo <remystuurwold@gmail.com>
2016-12-01 14:32:24 +0100

    Mushroom spores

---
+++ Assets/Personal Folders/Remy/Scripts/SporeTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SporeTrigger : MonoBehaviour {
+
+    [SerializeField]
+    private ParticleSystem m_SporeParticles = null;
+
+    void OnTriggerEnter(Collider col)
+    {
+        m_SporeParticles.Play();
+    }
+}
