b552095b7291ce5726c6520faeba21b9de2678bc
DESKTOP-1TPG7R2\Max <maxwrighton@gmail.com>
2016-12-02 19:21:20 +0100

    camera now follows rails

---
+++ Assets/Personal Folders/Max/CameraRail.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRail : MonoBehaviour {
+    [HideInInspector]
+    public Vector3[] m_PointsOnRail;
+
+    void Awake() {
+        RailPoint[] temp = this.GetComponentsInChildren<RailPoint>();
+        m_PointsOnRail = new Vector3[temp.Length];
+        for (int i = 0; i < temp.Length; i++) {
+            m_PointsOnRail[i] = temp[i].transform.position;
+            print(m_PointsOnRail[i]);
+        }
+    }
+}
