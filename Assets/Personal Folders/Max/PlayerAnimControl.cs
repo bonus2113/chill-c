6c6bf8fa0d03852837b67886b8d728119416efed
DESKTOP-1TPG7R2\Max <maxwrighton@gmail.com>
2016-12-03 01:15:55 +0100

    characer animation comopent added, needs to be hooked up

--- Assets/Personal Folders/Max/PlayerAnimControl.cs
+++ Assets/Personal Folders/Max/PlayerAnimControl.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class PlayerAnimControl : MonoBehaviour {
     private Animator m_Animator;
+    private bool b_Animating = false;
 
 	// Use this for initialization
 	void Start () {
-		if(this.GetComponent<Animator>() != null) {
-            m_Animator = this.GetComponent<Animator>();
-        }
+        m_Animator = this.GetComponent<Animator>();
 	}
-    void update() {
-        if (m_Animator.GetBool("Jump")) {
+    void Update () {
+        print("update");
+        if (b_Animating) {
             m_Animator.SetBool("Jump", false);
-        }
-        if (m_Animator.GetBool("Land")) {
             m_Animator.SetBool("Land", false);
+            b_Animating = false;
         }
     }
     public void PlayJumpAnim() {
         m_Animator.SetBool("Jump", true);
+        b_Animating = true;
     }
     public void PlayLandAnim() {
         m_Animator.SetBool("Land", true);
+        b_Animating = true;
     }
 }
