04e54f3bd1275ce654608b09c706a97d037917eb
jeichelbaum <trash.meil@gmx.de>
2016-12-03 12:38:39 +0100

    rune triggers door?

--- Assets/Personal Folders/Jery/Runes/RuneTrigger.cs
+++ Assets/Personal Folders/Jery/Runes/RuneTrigger.cs
@@ -4,14 +4,51 @@ using UnityEngine;
 
 public class RuneTrigger : MonoBehaviour {
 
+    static int runesActive = 0; 
+
     public bool triggered;
     public RuneTrigger triggers;
+    public GameObject triggerEffect;
 
 
-    float time = 0f;
-	void Update () {
-        time += Time.deltaTime;
-        float t = 1f + Mathf.Abs(Mathf.Sin(time)) * 0.7f;
-        GetComponent<MeshRenderer>().material.SetFloat("_Emission", t);
+	void Update () 
+    {
+
+         // update visuals
+        if(!triggered && triggers != null)
+        {
+            GetComponent<MeshRenderer>().material.SetFloat("_Emission", 1f + Mathf.Abs(Mathf.Sin(Time.time * 2f)) * 0.7f);
+        }
+        else if(triggered)
+        {
+            GetComponent<MeshRenderer>().material.SetFloat("_Emission", 1.4f + Mathf.Abs(Mathf.Sin(Time.time)) * 0.3f);
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.SetFloat("_Emission", 1.0f);
+        }
+
+        
+        if (!triggered && Vector3.Distance(Player.Instance.transform.position, transform.position) < 1f )
+        {
+            Trigger();
+        }
 	}
+
+    void Trigger()
+    {
+        runesActive++;
+        triggers.triggered = triggered = true;
+
+        if(runesActive == 2)
+        {
+            Player.Instance.b_CanOpenDoor = true;
+        }
+
+        if(triggerEffect != null)
+        {
+            var obj = (GameObject)GameObject.Instantiate(triggerEffect, transform);
+            obj.transform.localPosition = Vector3.zero;
+        }
+    }
 }
